6ca20fd5b3ef64efcb1892a29b2075428a85e2c7
Hernando José Peña Hidalgo <hernando.pena@unad.edu.co>
2017-11-04 09:35:29 -0500

    carga incial, continene clase para estudiantes

---
+++ ProyectoFormulario/ProyectoFormulario/Clases/Estudiante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clases.ProyectoFormulario
+{
+    public class Estudiante
+    {
+        public int codigo { get; set; }
+
+        public void registrar(int codigo)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
